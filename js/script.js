0982107b9c8c932b457c2547ec4e440d7b58e595
ashleighpresley <ashleighpresley1@gmail.com>
2021-10-27 14:39:03 -0500

    JS - Implementing Smooth Scroll for all Browsers

--- js/script.js
+++ js/script.js
@@ -16,6 +16,35 @@ navBtn.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
+//Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //Scoll to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //Scoll to selected section
+    if (href !== "#" && href.startsWith("#")) {
+      const section = document.querySelector(href);
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
